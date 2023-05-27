c2322d98662beaddb54e0d36605b02b01e0c3268
Pepijn Bakker <bakker.pepijn@gmail.com>
2023-05-27 17:02:55 +0200

    added omz

--- all.zsh
+++ all.zsh
@@ -6,5 +6,10 @@ source ./lsd.zsh
 source ./kitty_sshfix.zsh
 source ./sshpubkey.zsh
 source ./config_alias.zsh
+source ./omz.zsh
+
+export VISUAL=code
+export EDITOR=code
+export PAGER=most
 
 cd ~
---
+++ omz.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+export ZSH="$(pwd)/oh-my-zsh"
+
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
