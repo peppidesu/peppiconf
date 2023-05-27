6e6babef21a32d36ff2b267f42b9fcf7a1fbd30b
Pepijn Bakker <bakker.pepijn@gmail.com>
2023-05-27 17:13:01 +0200

    removed omz

--- all.zsh
+++ all.zsh
@@ -1,15 +1,11 @@
 #!/bin/zsh
-cd "$(dirname "$0")"
 
-source ./p10k.zsh
-source ./lsd.zsh
-source ./kitty_sshfix.zsh
-source ./sshpubkey.zsh
-source ./config_alias.zsh
-source ./omz.zsh
+source "$(dirname "$0")/p10k.zsh"
+source "$(dirname "$0")/lsd.zsh"
+source "$(dirname "$0")/kitty_sshfix.zsh"
+source "$(dirname "$0")/sshpubkey.zsh"
+source "$(dirname "$0")/config_alias.zsh"
 
 export VISUAL=code
 export EDITOR=code
 export PAGER=most
-
-cd ~
--- omz.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-export ZSH="$(pwd)/oh-my-zsh"
-
-plugins=(git)
-source $ZSH/oh-my-zsh.sh
